c922eb2f9a5f0ac731adab77e76b8084a5e0533f
Christeffer Torchia <christeffer.sethy@gmail.com>
2020-04-15 18:42:23 -0300

    Filter using CORS

--- src/app.js
+++ src/app.js
@@ -4,12 +4,26 @@ dotenv.config();
 
 import './database';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import studentRoutes from './routes/studentRoutes';
 import fileRoutes from './routes/fileRoutes';
+
+const whiteList = ['http://35.247.215.31', 'http://localhost:3000'];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not Allowed'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -19,6 +33,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(
--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -35,7 +35,7 @@ class TokenController {
       expiresIn: process.env.TOKEN_EXPIRATION,
     });
 
-    res.json({ token: token });
+    res.json({ token, user: { name: user.name, id, email } });
   }
 }
 export default new TokenController();
