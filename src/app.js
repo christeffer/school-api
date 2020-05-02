92d927e89c4beff1c4fc6db5d7b64ffc68f15f0c
Christeffer Torchia <christeffer.sethy@gmail.com>
2020-05-02 11:24:49 -0300

    fix link

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ class App {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(
-      '/images/',
+      '/api/images/',
       express.static(resolve(__dirname, '..', 'uploads', 'images'))
     );
   }
